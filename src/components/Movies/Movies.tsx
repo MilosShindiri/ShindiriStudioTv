23cce7109a08a9ecf2a8e2efae8f40b3f8e28abd
Milos_Petrovic <milos.petrovic@shindiristudio.com>
2025-10-15 12:08:28 +0200

    fixed background setting when going back from details page

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -8,9 +8,12 @@ import { rows } from "../../constants/rows";
 import { MoviesRow } from "../MovieRow/MovieRow";
 import type { Movie } from "../../types/movies";
 import { movies } from "../../constants/movies";
+import { useLocation } from "react-router-dom";
 
 export const MoviePage = () => {
   const { ref, focusKey } = useFocusable();
+  const location = useLocation();
+
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const onRowFocus = useCallback(
@@ -26,10 +29,20 @@ export const MoviePage = () => {
   const movieRow = rows.find((row) => row.title === "Movies");
 
   useEffect(() => {
+    const focusKey = location.state?.focusKey;
+    if (focusKey) {
+      const idString = focusKey.split("-")[1];
+      const movie = movies.find((m) => m.id === Number(idString));
+      if (movie) {
+        setSelectedMovie(movie);
+        return;
+      }
+    }
+
     if (!selectedMovie && movies.length > 0) {
       setSelectedMovie(movies[0]);
     }
-  }, [selectedMovie]);
+  }, [location.state?.focusKey, selectedMovie]);
 
   return (
     <FocusContext.Provider value={focusKey}>
